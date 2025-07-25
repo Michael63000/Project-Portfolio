564e10d9b9d78f9331df848ef56fd7e4fde84116
Michael Botros <mbotr020@uottawa.ca>
2025-07-25 18:34:48 -0400

    Second upload

---
+++ ELG 2336 - Arduino Coding for Project.c
@@ -0,0 +1,72 @@
+#include "Wire.h"
+
+#define HRMI_I2C_ADDR      127
+#define HRMI_HR_ALG        1   // 1= average sample, 0 = raw samp
+
+const int buzzer = 9; //buzzer to arduino pin 9
+
+void setup(){
+  setupHeartMonitor(HRMI_HR_ALG);
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop(){
+  int heartRate = getHeartRate();
+  Serial.println(heartRate); //May not need since it is to get a readable output on the computer
+
+  if (heartRate < 60 || heartRate > 100)
+  {
+      tone(buzzer, 1000); //Send 1KHz signal.
+      delay(3000); //For 3 seconds
+      noTone(buzzer); //Stop sound.
+  }
+  delay(1000); //just here to slow down the checking to once a second
+}
+
+void setupHeartMonitor(int type){
+  //setup the heartrate monitor
+  Wire.begin();
+  writeRegister(HRMI_I2C_ADDR, 0x53, type); // Configure the HRMI with the requested algorithm mode
+}
+
+int getHeartRate(){
+  //get and return heart rate
+  //returns 0 if we couldnt get the heart rate
+  byte i2cRspArray[3]; // I2C response array
+  i2cRspArray[2] = 0;
+
+  writeRegister(HRMI_I2C_ADDR,  0x47, 0x1); // Request a set of heart rate values
+
+  if (hrmiGetData(127, 3, i2cRspArray)) {
+    return i2cRspArray[2];
+  }
+  else{
+    return 0;
+  }
+}
+
+void writeRegister(int deviceAddress, byte address, byte val) {
+  //I2C command to send data to a specific address on the device
+  Wire.beginTransmission(deviceAddress); // start transmission to device
+  Wire.write(address);       // send register address
+  Wire.write(val);         // send value to write
+  Wire.endTransmission();     // end transmission
+}
+
+boolean hrmiGetData(byte addr, byte numBytes, byte* dataArray){
+  //Get data from heart rate monitor and fill dataArray byte with responce
+  //Returns true if it was able to get it, false if not
+  Wire.requestFrom(addr, numBytes);
+  if (Wire.available()) {
+
+    for (int i=0; i<numBytes; i++){
+      dataArray[i] = Wire.read();
+    }
+
+    return true;
+  }
+  else{
+    return false;
+  }
+}
